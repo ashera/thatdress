b8fd482c8b5dcf8474c40875e6e7fffeac4d8890
Adam Asher <ashera247@gmail.com>
2026-05-12 14:11:31 +1000

    Match /admin/users/[id] width to the nav bar (1280px via .page)

--- src/app/admin/users/[id]/page.tsx
+++ src/app/admin/users/[id]/page.tsx
@@ -358,7 +358,7 @@ export default async function AdminUserDetailPage({
   const isSuspended = !!user.suspended_at;
 
   return (
-    <div className="page admin-page" style={{ maxWidth: "100%" }}>
+    <div className="page admin-page">
       <Link href="/admin/users" className="back-link">
         ← All users
       </Link>
